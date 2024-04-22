e1df35fab161d9c9b02cce2b0742b82457689ca0
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-22 14:12:58 -0300

    Add project files.

---
+++ AcheParesEImpares/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] numbers = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Item {i + 1}: ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nValores pares: ");
+for (int i = 0; i < 10; i++)
+{
+    if ((numbers[i] % 2) == 0)
+    {
+        Console.WriteLine("- " + numbers[i]);
+    }
+}
+
+Console.WriteLine("\nValores impares: ");
+for (int i = 0; i < 10; i++)
+{
+    if ((numbers[i] % 2) != 0)
+    {
+        Console.WriteLine("- " + numbers[i]);
+    }
+}
+
+Console.WriteLine("Número na primeira posição: " + numbers[0]);
+Console.WriteLine("Número na última posição: " + numbers[9]);
